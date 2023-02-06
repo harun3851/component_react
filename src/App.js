346e31e9a9f503cf385027312bee9f352b302bdb
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-02-06 17:11:11 +0200

    fetch method

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import './App.css';
-import { useState} from 'react';
+import { useEffect, useState} from 'react';
 import {Text} from "./Text";
+import Axios from 'axios';
+
+
 
 function App() {
+// 1.medhot fetch("https://catfact.ninja/fact")
+//.then((res)=>res.json())
+//.then((data)=>{
+//console.log(data);
+//});
+ const fetchCatFact=()=>{
+ Axios.get("https://catfact.ninja/fact").then((res)=>{
+setCatFack(res.data.fact);
+  });
+  }
+  const [catFact,setCatFack]=useState(""); 
+  useEffect(()=>{
+    Axios.get("https://catfact.ninja/fact").then((res)=>{
+  setCatFack(res.data.fact);
+});
+
+  },[]);
+
   const [showText,setShowText]=useState(false);
   return (
     <div className="App">
@@ -13,6 +34,12 @@ function App() {
      > show text
      </button>
      {showText && <Text />}
+     <br></br>
+     <br></br>
+     <br></br>
+
+     <button onClick={fetchCatFact}>generate cat fact</button>
+     <p>{catFact}</p>
     </div>
   );
 }
